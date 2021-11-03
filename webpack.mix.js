5b0e5f91a557d22b2f92d29d0591a6fdd54df4f6
vnext-tuanna <tuanna3@vnext.com.vn>
2021-11-03 13:35:58 +0700

    feat: Add bootstrap

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,18 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
-const publicCssFolder = 'public/css';
-const publicJsFolder = 'public/js';
-const publicFontFolder = 'public/fonts';
 
-mix.js('resources/js/app.js', publicJsFolder)
-    .sass('resources/scss/app.scss', publicCssFolder);
-
-
-mix.options({
-    processCssUrls: false
-});
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
